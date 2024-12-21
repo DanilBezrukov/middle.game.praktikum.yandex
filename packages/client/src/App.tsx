94cec99bded3a335b3334c84bb91009d21ed097a
Sergy Yakovenko <sergeyyakovenko15@gmail.com>
2024-12-21 14:08:51 +0500

    Добавил правила для eslint, настроил hysky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,20 @@
-import { useEffect } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  useEffect(() => {
-    const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
+  // **** Закоментировал, пока что нет сервера ***
 
-    fetchServerData()
-  }, [])
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>
+  // useEffect(() => {
+  //   const fetchServerData = async () => {
+  //     const url = `http://localhost:${__SERVER_PORT__}`;
+  //     const response = await fetch(url);
+  //     const data = await response.json();
+  //     console.log(data);
+  //   };
+  //
+  //   fetchServerData();
+  // }, []);
+
+  return <div className="App">Вот тут будет жить ваше приложение :)</div>;
 }
 
-export default App
+export default App;
