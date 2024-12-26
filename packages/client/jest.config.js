c3fb71393e1759bab0b3fdeabbf8606a764992ea
Ivan Fabrichnov <ifabrichnov@yandex.ru>
2024-12-26 18:47:52 +0300

    FRO-34 fix linter

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -10,6 +10,7 @@ export default {
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$": 'identity-obj-proxy',
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$":
+      'identity-obj-proxy',
   },
 };
