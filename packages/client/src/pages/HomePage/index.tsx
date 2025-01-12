05595efac5026f665baef177cea94facbe345bf7
Feenny <kolomievskayazhenya@gmail.com>
2025-01-13 00:02:43 +0300

    удален файл стилей

--- packages/client/src/pages/HomePage/index.tsx
+++ packages/client/src/pages/HomePage/index.tsx
@@ -5,9 +5,8 @@ import { paths } from "@/app/constants/paths";
 import { UiButton } from "@/components/ui/UiButton";
 import { UiLayout } from "@/components/ui/UiLayout";
 import { UiPaper } from "@/components/ui/UiPaper";
-import { ProfileAvatar, ProfileInfo } from "@/pages/ProfilePage/ProfileFeatures";
+import { ProfileAvatar } from "@/pages/ProfilePage/ProfileFeatures";
 import gameIcon from "@/assets/game-icon.png";
-import styles from "./HomePage.module.css";
 
 export function HomePage() {
   const navigate = useNavigate();
@@ -26,62 +25,93 @@ export function HomePage() {
         <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
           HOME PAGE
         </Typography>
-        <UiPaper className={styles.HomePage_UiPaper}>
-          <div className={styles.HomeProfile_Container}>
-            <ProfileAvatar></ProfileAvatar>
-            <div className={styles.HomeProfile_Buttons}>
+        <UiPaper sx={{ mb: 2, p: { xs: 3, sm: 5 } }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}>
+            <ProfileAvatar />
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+              }}>
               <UiButton onClick={() => navigate(paths.profile)}>Страница Профиля</UiButton>
               <UiButton onClick={handleLogout}>Выход</UiButton>
-            </div>
-          </div>
+            </Box>
+          </Box>
         </UiPaper>
-        <UiPaper className={styles.HomePage_UiPaper}>
-          {" "}
+        <UiPaper sx={{ mb: 2, p: { xs: 3, sm: 5 } }}>
           <nav>
-            <ul className={styles.NavigationList}>
-              <li>
+            <Box
+              component="ul"
+              sx={{
+                listStyle: "none",
+                p: 0,
+                m: 0,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 5,
+              }}>
+              <Box
+                component="li"
+                sx={{ textAlign: "center", cursor: "pointer" }}
+                onClick={() => navigate(paths.forum)}>
                 <Typography
                   variant="body1"
                   sx={{
-                    marginTop: 2,
-                    textAlign: "center",
-                    cursor: "pointer",
-                    color: "#000000",
+                    mt: 2,
+                    color: "#000",
                     fontWeight: "bold",
                     textDecoration: "underline",
-                  }}
-                  onClick={() => navigate(paths.forum)}>
+                  }}>
                   Форум
                 </Typography>
-              </li>
-              <li>
+              </Box>
+              <Box
+                component="li"
+                sx={{ textAlign: "center", cursor: "pointer" }}
+                onClick={() => navigate(paths.leaderboard)}>
                 <Typography
                   variant="body1"
                   sx={{
-                    marginTop: 2,
-                    textAlign: "center",
-                    cursor: "pointer",
-                    color: "#000000",
+                    mt: 2,
+                    color: "#000",
                     fontWeight: "bold",
                     textDecoration: "underline",
-                  }}
-                  onClick={() => navigate(paths.leaderboard)}>
+                  }}>
                   Лидеры
                 </Typography>
-              </li>
-            </ul>
+              </Box>
+            </Box>
           </nav>
         </UiPaper>
-        <UiPaper className={styles.HomePage_UiPaper}>
-          <div className={styles.GameDescription}>
-            <img src={gameIcon} className={styles.gameIcon} alt="Game icon Flappy Bird" />
+        <UiPaper sx={{ mb: 2, p: { xs: 3, sm: 5 } }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              gap: 7,
+            }}>
+            <Box
+              component="img"
+              src={gameIcon}
+              alt="Game icon Flappy Bird"
+              sx={{ maxWidth: "100px" }}
+            />
             <Typography component="h6" sx={{ mb: 2 }}>
               <strong>Flappy Bird</strong> — это простая аркадная игра, разработанная в 2013 году
               вьетнамским разработчиком Нгуен Ха Донг (Nguyen Ha Dong). Игра приобрела огромную
               популярность благодаря своей простоте и сложным механикам, несмотря на минималистичную
               графику и геймплей.
             </Typography>
-          </div>
+          </Box>
           <UiButton onClick={() => navigate(paths.game)}>Играть</UiButton>
         </UiPaper>
       </Container>
