b1d4aba0e2dd9fc7967f60c90322fa5bec12f817
Feenny <kolomievskayazhenya@gmail.com>
2025-01-19 19:42:54 +0300

    переиспользованы компоненты, добавлен переход на кнопках

--- packages/client/src/pages/ErrorPage400/index.tsx
+++ packages/client/src/pages/ErrorPage400/index.tsx
@@ -1,95 +1,81 @@
 import React from "react";
-import { Box, Button, Paper, Typography } from "@mui/material";
+import { paths } from "@/app/constants/paths";
+import { useNavigate } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import { UiButton } from "@/components/ui/UiButton";
+import { UiLayout } from "@/components/ui/UiLayout";
+import { UiPaper } from "@/components/ui/UiPaper";
 import errorIcon from "@/assets/error404hamster.png";
-import backgroundImage from "@/assets/background.png";
 
-export const ErrorPage400: React.FC = () => (
-  <Box
-    sx={{
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      height: "100vh",
-      backgroundColor: "#f5f5f5",
-      backgroundImage: `url(${backgroundImage})`,
-      textAlign: "center",
-      padding: 3,
-    }}>
-    <Paper
-      sx={{
-        padding: "70px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: 750,
-        maxWidth: "100%",
-        overflow: "auto",
-        borderRadius: 3,
-        backdropFilter: "blur(10px)",
-        backgroundColor: "rgba(255, 255, 255, 0.8)",
-      }}
-      elevation={1}>
-      <Box
+export const ErrorPage400: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <UiLayout>
+      <UiPaper
         sx={{
-          position: "relative",
-          width: "60%",
           display: "flex",
+          flexDirection: "column",
           alignItems: "center",
-          justifyContent: "center",
-          marginBottom: "20px",
+          width: 750,
+          maxWidth: "100%",
+          borderRadius: 3,
+          backgroundColor: "rgba(255, 255, 255, 0.8)",
         }}>
-        <img
-          src={errorIcon}
-          alt="Error 404"
-          style={{
-            position: "absolute",
-            left: 0,
-            height: "100px",
-            marginRight: "15px",
-          }}
-        />
-        <Typography
-          variant="h1"
+        <Box
           sx={{
-            fontSize: "4rem",
+            position: "relative",
+            width: "60%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginBottom: "20px",
+          }}>
+          <img
+            src={errorIcon}
+            alt="Error 400"
+            style={{
+              position: "absolute",
+              left: 0,
+              height: "100px",
+              marginRight: "15px",
+            }}
+          />
+          <Typography
+            variant="h1"
+            sx={{
+              fontSize: "4rem",
+              fontWeight: "bold",
+            }}>
+            404
+          </Typography>
+        </Box>
+        <Typography
+          variant="h4"
+          sx={{
+            fontSize: "32px",
             fontWeight: "bold",
           }}>
-          404
+          Ошибка!
         </Typography>
-      </Box>
-      <Typography
-        variant="h4"
-        sx={{
-          fontSize: "32px",
-          fontWeight: "bold",
-        }}>
-        Ошибка!
-      </Typography>
-      <Typography
-        variant="h6"
-        sx={{
-          fontSize: "18px",
-          marginBottom: 3,
-        }}>
-        Кажется, такой страницы нет...
-      </Typography>
-      <Button
-        variant="contained"
-        type="submit"
-        sx={{
-          width: 400,
-          height: 55,
-          backgroundColor: "#FFE600",
-          color: "#000",
-          borderRadius: 3,
-          marginTop: "30px",
-          typography: {
-            fontSize: "17px",
-            fontWeight: "bold",
-          },
-        }}>
-        Вернуться
-      </Button>
-    </Paper>
-  </Box>
-);
+        <Typography
+          variant="h6"
+          sx={{
+            fontSize: "18px",
+            marginBottom: 3,
+          }}>
+          Кажется, такой страницы нет...
+        </Typography>
+        <UiButton
+          sx={{
+            width: 400,
+            height: 55,
+            marginTop: "50px",
+          }}
+          onClick={() => navigate(paths.homePage)}>
+          Вернуться
+        </UiButton>
+      </UiPaper>
+    </UiLayout>
+  );
+};
