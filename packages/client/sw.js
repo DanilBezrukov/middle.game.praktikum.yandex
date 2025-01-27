91703bb2dc7259e3a0174e69d64f9bc15a9764c4
Ivan Fabrichnov <ifabrichnov@yandex.ru>
2025-01-27 23:17:21 +0300

    FRO-70 исправление Service Workers

--- packages/client/sw.js
+++ packages/client/sw.js
@@ -2,43 +2,39 @@ const CACHE_PREFIX = 'flappy-bird';
 const CACHE_VERSION = 1;
 const CACHE_NAME = `${CACHE_PREFIX}-v${CACHE_VERSION}`;
 
-
-const URLS = [
-  '/index.html',
-  'src/assets/background.png',
-  'src/assets/background-game.png',
-  'src/assets/error404hamster.png',
-  'src/assets/error-bird.png',
-  'src/assets/game-icon.png',
-  'src/assets/obstacle-to-game.png',
-  'src/assets/fonts/JollyLodger-Regular.woff',
-  'src/assets/birds/bird-wings-down.png',
-  'src/assets/birds/bird-wings-down-v2.png',
-  'src/assets/birds/bird-wings-down-v3.png',
-  'src/assets/birds/bird-wings-up.png',
-  'src/assets/birds/bird-wings-up-v2.png',
-  'src/assets/birds/bird-wings-up-v3.png',
-];
-
-
 this.addEventListener('install', event => {
   //eslint-disable-next-line no-console
   console.log("Service Worker: Установлен");
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    fetch('./manifest.json')
+      .then(response => response.json())
+      .then(manifest => {
+        const URLS = [
+          manifest['index.html'],
+          manifest['src/assets/background.png'].file,
+          manifest['src/assets/background-game.png'].file,
+          manifest['src/assets/error404hamster.png'].file,
+          manifest['src/assets/error-bird.png'].file,
+          manifest['src/assets/game-icon.png'].file,
+          manifest['src/assets/obstacle-to-game.png'].file,
+          manifest['src/assets/fonts/JollyLodger-Regular.woff'].file,
+          manifest['src/assets/birds/bird-wings-down.png'].file,
+          manifest['src/assets/birds/bird-wings-down-v2.png'].file,
+          manifest['src/assets/birds/bird-wings-down-v3.png'].file,
+          manifest['src/assets/birds/bird-wings-up.png'].file,
+          manifest['src/assets/birds/bird-wings-up-v2.png'].file,
+          manifest['src/assets/birds/bird-wings-up-v3.png'].file,
+        ];
+
+        return caches.open(CACHE_NAME).then(cache => {
+          //eslint-disable-next-line no-console
+          console.log("Кэширование файлов:", URLS);
+          return cache.addAll(URLS.map(url => `/${url}`));
+        });
+      })
       //eslint-disable-next-line no-console
-      console.log("Кэширование файлов...");
-      return Promise.all(URLS.map(url =>
-        fetch(url).then(response => {
-          if (!response.ok) {
-            //eslint-disable-next-line no-console
-            console.error(`Не удалось загрузить файл: ${url}`);
-            throw new Error(`Не удалось загрузить файл: ${url}`);
-          }
-          return cache.put(url, response);
-        })
-      ));
-    })
+      .catch(err => console.error("Ошибка загрузки манифеста:", err))
   );
 });
 
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   },
   plugins: [react()],
   build: {
+    manifest: true,
     rollupOptions: {
       input: {
         app: "./index.html",
