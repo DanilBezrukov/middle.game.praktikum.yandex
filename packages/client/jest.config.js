8c63a096f37477f3113b976a23f65be80b4440e7
Danil Bezrukov <danil.bezrukov@icloud.com>
2025-02-17 18:27:54 +0300

    FRO-57 - Настройка Express для SSR (без Redux и Router) | фикс jest

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,4 +1,5 @@
-import dotenv from "dotenv";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require("dotenv");
 dotenv.config();
 
 export default {
@@ -9,8 +10,8 @@ export default {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    "^@/(.*)$": "<rootDir>/src/$1",
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$":
-      'identity-obj-proxy',
+      "identity-obj-proxy",
   },
 };
