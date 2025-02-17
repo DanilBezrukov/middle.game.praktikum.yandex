fed6cfb888b08f06fb0a41f7aca893ad38c3d220
Danil Bezrukov <danil.bezrukov@icloud.com>
2025-02-17 18:32:34 +0300

    FRO-57 - Настройка Express для SSR (без Redux и Router) | фикс jest

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -11,6 +11,7 @@
 //   },
 //   moduleNameMapper: {
 //     "^@/(.*)$": "<rootDir>/src/$1",
+// eslint-disable-next-line max-len
 //     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$":
 //       "identity-obj-proxy",
 //   },
