6c07b02c16b588f63d0e11a319af59089d218841
Danil Bezrukov <danil.bezrukov@icloud.com>
2025-02-17 18:29:45 +0300

    FRO-57 - Настройка Express для SSR (без Redux и Router) | фикс jest

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,17 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const dotenv = require("dotenv");
-dotenv.config();
-
-export default {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  testMatch: ["<rootDir>/src/**/*.test.{ts,tsx}"],
-  globals: {
-    __SERVER_PORT__: process.env.SERVER_PORT,
-  },
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$":
-      "identity-obj-proxy",
-  },
-};
+// const dotenv = require("dotenv");
+// dotenv.config();
+//
+// export default {
+//   preset: "ts-jest",
+//   testEnvironment: "jsdom",
+//   testMatch: ["<rootDir>/src/**/*.test.{ts,tsx}"],
+//   globals: {
+//     __SERVER_PORT__: process.env.SERVER_PORT,
+//   },
+//   moduleNameMapper: {
+//     "^@/(.*)$": "<rootDir>/src/$1",
+//     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|css)$":
+//       "identity-obj-proxy",
+//   },
+// };
